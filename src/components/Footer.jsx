a51e8d906f017ce54c30216434878fd005798bef
Nipun Rustagi <nipun.rustagi1@gmail.com>
2023-01-05 16:37:39 -0500

    Upsated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = (props) => {
         <i className="fas fa-code" /> by{" "}
         <a
           rel="noopener"
-          href="https://github.com/hashirshoaeb"
+          href="https://github.com/Nipun0309"
           aria-label="My GitHub"
         > <span className="badge bg-dark">
             Nipun Rustagi
